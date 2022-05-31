b3541dbf8dbd5bf2f8041cd1e382eb09214b55d3
Maria Eduarda <mariaedu9434@gmail.com>
2022-05-31 20:50:47 -0300

    Lab list

--- Program.cs
+++ Program.cs
@@ -59,4 +59,45 @@ if(modelName == "Computer")
 
         Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
     }
-}  
+
+    if(modelName == "Lab")
+    {
+        if(modelAction == "List")
+        {
+            Console.WriteLine("Lab List");
+            var connection = new SqliteConnection("Data Source=database.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Lab";
+
+            var reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3));
+            }
+            connection.Close();
+        }
+         
+         if(modelAction == "New")
+        {
+            var id = Convert.ToInt32(args[2]);
+            var number = args[3];
+            string name = args[4];
+            string block = args[5];
+
+            var connection = new SqliteConnection("Data Source=database.db");
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block)";
+            command.Parameters.AddWithValue("$id", id);
+            command.Parameters.AddWithValue("$number", number);
+            command.Parameters.AddWithValue("$name", name);
+            command.Parameters.AddWithValue("$block", block);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
